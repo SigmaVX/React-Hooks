10a8660395adea930ecd1e27ad67115cdb056efb
Tony Wible <34351153+SigmaVX@users.noreply.github.com>
2020-01-22 15:14:27 -0500

    added comments to store

--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -1,5 +1,10 @@
 import React, { useState, createContext } from "react";
 
+// Build Two Way Store By:
+// Creating Context, Provider, and Consumer
+// Use useState To Update Store By Passing 'set' Methods To Provider
+// Note: Consumer Not Really Used Much As One Can use useContext(AppStore) instead
+
 let store = {
   luckyText: "FooBarBaz",
   totalClicks: 0,
@@ -10,7 +15,6 @@ let AppStore = createContext(store);
 
 const AppStoreProvider = props => {
   const [appStore, setAppStore] = useState(store);
-  console.log(appStore);
   return (
     <AppStore.Provider value={appStore}>{props.children}</AppStore.Provider>
   );
